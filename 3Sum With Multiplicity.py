25f2dab3403009823e5b0a83b145d0103138a517
Abhinav Mathur <32503976+abhinav1912@users.noreply.github.com>
2022-04-06 21:15:36 +0530

    Counter + Combinatorics

---
+++ 3Sum With Multiplicity.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        cnt, ans = Counter(arr), 0
+        for i in cnt:
+            for j in cnt:
+                k = target - i - j
+                if i == j == k:
+                    ans += cnt[i] * (cnt[i] - 1) * (cnt[i] - 2) // 6
+                elif i == j:
+                    ans += cnt[i] * (cnt[i] - 1) // 2 * cnt[k]
+                elif i < j < k:
+                    ans += cnt[i] * cnt[j] * cnt[k]
+        return ans % (10**9 + 7)
